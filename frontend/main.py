63c922fc47fd07e803e5f236c537cec08b64e2e5
Maximilian Gartz <maximilian.gartz@ml6.eu>
2024-02-17 18:32:47 +0100

    Simplify frontend.

--- frontend/main.py
+++ frontend/main.py
@@ -2,36 +2,24 @@ import json
 import os
 from typing import Dict, Any
 
+import boto3
 import gradio as gr
-from transformers import pipeline
 
 SAGEMAKER_ENDPOINT = os.environ.get("SAGEMAKER_ENDPOINT")
 SAGEMAKER_REGION = os.environ.get("SAGEMAKER_REGION")
 
+session = boto3.Session()
+runtime = session.client(service_name="sagemaker-runtime", region_name=SAGEMAKER_REGION)
 
-if SAGEMAKER_ENDPOINT and SAGEMAKER_REGION:
-    import boto3
-    session = boto3.Session()
-    runtime = session.client(service_name="sagemaker-runtime", region_name=SAGEMAKER_REGION)
 
-    def fn(tweet: str) -> Dict[str, Any]:
-        response = runtime.invoke_endpoint(
-            EndpointName=SAGEMAKER_ENDPOINT,
-            ContentType="application/json",
-            Body=json.dumps(dict(inputs=tweet, parameters=dict(return_all_scores=True)))
-        )
-        preds = json.loads(response["Body"].read().decode("utf-8"))
-        return {pred["label"]: pred["score"] for pred in preds[0]}
-else:
-    pipe = pipeline(
-        task="text-classification",
-        model="max-gartz/distilbert-tweet_eval-emotion",
-        return_all_scores=True
+def fn(tweet: str) -> Dict[str, Any]:
+    response = runtime.invoke_endpoint(
+        EndpointName=SAGEMAKER_ENDPOINT,
+        ContentType="application/json",
+        Body=json.dumps(dict(inputs=tweet, parameters=dict(return_all_scores=True)))
     )
-
-    def fn(tweet: str) -> Dict[str, Any]:
-        preds = pipe(tweet)
-        return {pred["label"]: pred["score"] for pred in preds[0]}
+    preds = json.loads(response["Body"].read().decode("utf-8"))
+    return {pred["label"]: pred["score"] for pred in preds[0]}
 
 
 interface = gr.Interface(
